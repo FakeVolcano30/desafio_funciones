831c15c78e9a564b28a36b5c27307d41f0ff9ce9
Jose Estay <estayjose3@gmail.com>
2026-04-11 01:28:57 -0400

    faltan agregar cosas

---
+++ assets/js/1_funcion.js
@@ -0,0 +1,13 @@
+function operacion(a,b){ 
+    return a + b;
+
+}
+function suma(a,b){
+    return a + b;
+}
+operacion(2,3, suma)
+
+function mostrarResultado(){
+    return operacion(2,3, suma);
+}
+
---
+++ assets/js/3_pintar.js
@@ -0,0 +1,7 @@
+document.getElementById("ele1").style.backgroundColor = 'green';
+
+function pintar(){
+ele.style.backgroundColor = 'yellow'
+}
+const ele = document.getElementById("ele1")
+ele.addEventListener("click", pintar);
