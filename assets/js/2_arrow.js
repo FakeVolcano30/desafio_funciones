663b6dc5d1cef39871d0f1f2ed8b1ed0a57398a3
Jose Estay <estayjose3@gmail.com>
2026-04-12 20:40:36 -0400

    esta listo

---
+++ assets/js/2_arrow.js
@@ -0,0 +1,5 @@
+/*let suma = function(a, b){
+return a + b
+}
+funcion antes de ser cambiada a arrow function*/
+let suma = (a, b) => a + b;
--- assets/js/4_colores.js
+++ assets/js/4_colores.js
@@ -1,11 +1,36 @@
-/*
-Dentro del script agrega el evento para que al hacer click a cada uno de los
-divs, este cambie de color al color negro. Utiliza addEventListener para
-agregar el evento. */
-// estoy es de las cajas de box1, box2, etc del style.css
 function pintar(){
 box1.style.backgroundColor = 'black'
 }
 const box1 = document.getElementById("box1")
 box1.addEventListener("click", pintar);
 
+document.addEventListener('keydown', function (event) {
+if (event.key === 'a') {
+key.style.backgroundColor = 'pink'
+} else if (event.key === 's') {
+key.style.backgroundColor = 'orange'
+} else if (event.key === 'd') {
+key.style.backgroundColor = 'lightblue'
+}
+});
+
+const contenedorCajas = document.getElementById("cajas");
+
+document.addEventListener('keydown', function(event) {
+    let newColor;
+
+    if (event.key === 'q') {
+        newColor = 'purple';
+    } else if (event.key === 'w') {
+        newColor = 'gray';
+    } else if (event.key === 'e') {
+        newColor = 'brown';
+    }
+
+    if (newColor) {
+        const nuevoDiv = document.createElement("div");
+        nuevoDiv.classList.add("nueva-caja");
+        nuevoDiv.style.backgroundColor = newColor;
+        contenedorCajas.appendChild(nuevoDiv);
+    }
+});
